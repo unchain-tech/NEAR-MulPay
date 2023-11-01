1a6846af2f9c064e42cfed8de9352ca270405ac6
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-11-01 21:30:12 +0900

    feat: update send transaction

--- packages/client/lib/model/contract_model.dart
+++ packages/client/lib/model/contract_model.dart
@@ -9,6 +9,7 @@ import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
 import 'package:url_launcher/url_launcher_string.dart';
 import 'package:walletconnect_flutter_v2/walletconnect_flutter_v2.dart';
+import 'package:web3dart/crypto.dart';
 import 'package:web3dart/web3dart.dart';
 import 'package:web_socket_channel/io.dart';
 
@@ -144,29 +145,59 @@ class ContractModel extends ChangeNotifier {
     return result;
   }
 
-  // Future<void> sendTransaction(String contractName, String contractAddress,
-  //     String functionName, List<dynamic> args) async {
-  //   if (_connection != null && _client != null) {
-  //     final contract = await getContract(contractName, contractAddress);
-  //     ContractFunction function = contract.function(functionName);
-  //     final transaction = Transaction.callContract(
-  //       contract: contract,
-  //       function: function,
-  //       from: EthereumAddress.fromHex(_connection!.account),
-  //       parameters: args,
-  //     );
-  //     final tra = _client!.sendTransaction(
-  //         _connection!.credentials, transaction,
-  //         chainId: 1313161555);
-  //     if (!await launchUrlString(_deepLink)) {
-  //       throw "Could not launch $_deepLink";
-  //     }
-  //     await tra;
-  //     notifyListeners();
-  //   } else {
-  //     print("There is no connection to wallet or no client");
-  //   }
-  // }
+  Future<EthereumTransaction> generateTransaction(
+      String contractName,
+      String contractAddress,
+      String functionName,
+      List<dynamic> parameters) async {
+    debugPrint('===== In generateTransaction ====='); // TODO: delete
+    debugPrint('=== contractName: $contractName'); // TODO: delete
+    debugPrint('=== contractAddress: $contractAddress'); // TODO: delete
+    debugPrint('=== functionName: $functionName'); // TODO: delete
+    debugPrint('=== parameters: $parameters'); // TODO: delete
+
+    _contract = await getContract(contractName, contractAddress);
+    debugPrint('=== _contract: $_contract'); // TODO: delete
+
+    ContractFunction function = _contract!.function(functionName);
+
+    debugPrint('=== function: $function'); // TODO: delete
+
+    // web3dartを使用して、トランザクションを作成します。
+    final Transaction transaction = Transaction.callContract(
+        contract: _contract!, function: function, parameters: parameters);
+
+    debugPrint('=== transaction: $transaction'); // TODO: delete
+
+    // walletconnect_flutter_v2を使用して、Ethereumトランザクションを作成します。
+    final EthereumTransaction ethereumTransaction = EthereumTransaction(
+      from: _account!,
+      to: contractAddress,
+      value: '0x${transaction.value?.getInWei.toRadixString(16) ?? '0'}',
+      data: transaction.data != null ? bytesToHex(transaction.data!) : null,
+    );
+
+    debugPrint('=== ethereumTransaction: $ethereumTransaction'); // TODO: delete
+
+    return ethereumTransaction;
+  }
+
+  Future<String> sendTransaction(EthereumTransaction transaction) async {
+    // Metamaskアプリケーションを起動します。
+    await launchUrlString(_deepLinkUrl!, mode: LaunchMode.externalApplication);
+
+    // 署名をリクエストします。
+    final String signResponse = await _wcClient?.request(
+      topic: _sessionData?.topic ?? "",
+      chainId: 'eip155:1313161555',
+      request: SessionRequestParams(
+        method: 'eth_sendTransaction',
+        params: [transaction.toJson()],
+      ),
+    );
+
+    return signResponse;
+  }
 
   Future<void> setConnection(
       String deepLinkUrl, Web3App wcClient, SessionData sessionData) async {
@@ -200,47 +231,88 @@ class ContractModel extends ChangeNotifier {
     return result[0].toString();
   }
 
-  // Future<bool> getTokensInfo() async {
-  //   for (int i = 0; i < tokenList.length; i++) {
-  //     final balance =
-  //         await getBalance(tokenList[i].contractName, tokenList[i].address);
-  //     final ethValue = await getEthBalance(tokenList[i].address);
-  //     final ethBalance =
-  //         ((double.parse(ethValue) * double.parse(balance) / (pow(10, 18)))
-  //                 .ceil())
-  //             .toString();
-  //     tokenList[i]
-  //       ..balance = balance
-  //       ..ethBalance = ethBalance;
-  //   }
-  //   return true;
-  // }
+  Future<bool> getTokensInfo() async {
+    for (int i = 0; i < tokenList.length; i++) {
+      final balance =
+          await getBalance(tokenList[i].contractName, tokenList[i].address);
+      final ethValue = await getEthBalance(tokenList[i].address);
+      final ethBalance =
+          ((double.parse(ethValue) * double.parse(balance) / (pow(10, 18)))
+                  .ceil())
+              .toString();
+      tokenList[i]
+        ..balance = balance
+        ..ethBalance = ethBalance;
+    }
+    return true;
+  }
 
-  // Future<void> sendToken(String sendTokenContractName, String sendTokenAddress,
-  //     String receiveTokenAddress, String recipientAddress, int amount) async {
-  //   await sendTransaction(
-  //     sendTokenContractName,
-  //     sendTokenAddress,
-  //     "approve",
-  //     [
-  //       EthereumAddress.fromHex(dotenv.env['SWAP_CONTRACT_ADDRESS']!),
-  //       BigInt.from(amount),
-  //     ],
-  //   );
-  //   await sendTransaction(
-  //     dotenv.env['SWAP_CONTRACT_NAME']!,
-  //     dotenv.env['SWAP_CONTRACT_ADDRESS']!,
-  //     "swap",
-  //     [
-  //       // measureToken is got rid of
-  //       EthereumAddress.fromHex(sendTokenAddress),
-  //       EthereumAddress.fromHex(sendTokenAddress),
-  //       EthereumAddress.fromHex(receiveTokenAddress),
-  //       BigInt.from(amount),
-  //       EthereumAddress.fromHex(recipientAddress),
-  //     ],
-  //   );
-  // }
+  Future<void> sendToken(String sendTokenContractName, String sendTokenAddress,
+      String receiveTokenAddress, String recipientAddress, int amount) async {
+    try {
+      // トークンの送信を承認します。
+      final EthereumTransaction ethereumTransactionOfApprove =
+          await generateTransaction(
+              sendTokenContractName, sendTokenAddress, "approve", [
+        EthereumAddress.fromHex(dotenv.env['SWAP_CONTRACT_ADDRESS']!),
+        BigInt.from(amount),
+      ]);
+
+      final String signResponseOfApprove =
+          await sendTransaction(ethereumTransactionOfApprove);
+      debugPrint(
+          '=== signResponseOfApprove: $signResponseOfApprove'); // TODO: delete
+
+      final EthereumTransaction ethereumTransactionOfSwap =
+          await generateTransaction(
+        dotenv.env['SWAP_CONTRACT_NAME']!,
+        dotenv.env['SWAP_CONTRACT_ADDRESS']!,
+        "swap",
+        [
+          // measureToken is got rid of
+          EthereumAddress.fromHex(sendTokenAddress),
+          EthereumAddress.fromHex(sendTokenAddress),
+          EthereumAddress.fromHex(receiveTokenAddress),
+          BigInt.from(amount),
+          EthereumAddress.fromHex(recipientAddress),
+        ],
+      );
+
+      final String signResponseOfSwap =
+          await sendTransaction(ethereumTransactionOfSwap);
+      debugPrint('=== signResponseOfSwap: $signResponseOfSwap'); // TODO: delete
+    } catch (error) {
+      debugPrint('=== error: $error'); // TODO: delete
+    } finally {
+      notifyListeners();
+    }
+  }
+
+  Future<void> distributeToken(String tokenAddress) async {
+    try {
+      debugPrint('=== tokenAddress: $tokenAddress'); // TODO: delete
+      final EthereumTransaction ethereumTransaction = await generateTransaction(
+        dotenv.env["SWAP_CONTRACT_NAME"]!,
+        dotenv.env["SWAP_CONTRACT_ADDRESS"]!,
+        "distributeToken",
+        [
+          EthereumAddress.fromHex(tokenAddress),
+          BigInt.from(100),
+          EthereumAddress.fromHex(_account!),
+        ],
+      );
+
+      debugPrint(
+          '=== ethereumTransaction: $ethereumTransaction'); // TODO: delete
+
+      final String signResponse = await sendTransaction(ethereumTransaction);
+      debugPrint('=== signResponse: $signResponse'); // TODO: delete
+    } catch (error) {
+      rethrow;
+    } finally {
+      notifyListeners();
+    }
+  }
 
   Future<double> getTotalBalance() async {
     double total = 0;
@@ -277,4 +349,26 @@ class Token {
     this.balance = "0";
     this.ethBalance = "0";
   }
+}
+
+// Ethereumトランザクションを作成するためのモデルクラス
+class EthereumTransaction {
+  const EthereumTransaction({
+    required this.from,
+    required this.to,
+    required this.value,
+    this.data,
+  });
+
+  final String from;
+  final String to;
+  final String value;
+  final String? data;
+
+  Map<String, dynamic> toJson() => {
+        'from': from,
+        'to': to,
+        'value': value,
+        'data': data,
+      };
 }
